9a308da761a00ceaf100167b54cba08a078c7a7e
CynDev <cynthiadevcreations@gmail.com>
2021-01-29 16:30:51 +0100

    mise à jour du fichier js

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const menu = document.querySelector('.liste-nav');
 btnMenu.addEventListener('click', function(){
     menu.classList.toggle('active');
 })
+
+const allLinks = document.querySelectorAll('.item-nav');
+
+allLinks.forEach(function(item){
+    item.addEventListener('click', function(){
+        menu.classList.toggle('active');
+    })
+})
